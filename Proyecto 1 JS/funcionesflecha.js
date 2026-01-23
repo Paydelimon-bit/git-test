df306e6628f42fd441d463c9511b6f475c5a255f
Saul Odan <yosoberana1@gmail.com>
2026-01-23 17:50:43 -0600

    Algo

---
+++ Proyecto 1 JS/funcionesflecha.js
@@ -0,0 +1,9 @@
+let calcularArea=(ancho,alto)=>ancho*alto;
+
+let esPar = n => n%2===0;
+
+let preciosUSD=[250, 300, 450];
+
+let preciosMX=preciosUSD.map(moneda=>moneda*20);
+
+console.log(preciosMX);
